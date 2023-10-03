ecab268fbab27079a06054415821ae4e49ca9208
MakiWolf <makiwolf@outlook.at>
2023-10-04 00:07:46 +0200

    add unit test

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,20 @@
+using Rechner5;
+using System;
+using NUnit.Framework;
+
+
+namespace TestProject
+{
+    
+    public class Tests
+    {
+        [TestCase]
+        public void Test2()
+        {
+            calculation_class a = new calculation_class();
+            double b = a.add(1, 8);
+            Assert.AreEqual(b, 9.0);
+
+        }
+    }
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,24 @@
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        [Test]
+        public void Test2()
+        {
+            
+            //calculation_class calc = new calculation_class();
+            //erg = calc.add(zahl1, zahl2);
+        }
+    }
+}
