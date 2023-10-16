6a6ce02d9b97516516cf6483dd2d060ddf2b61d4
MakiWolf <makiwolf@outlook.at>
2023-10-16 19:24:11 +0200

    add unit tests

--- TestProject/UnitTest1.cs
+++ TestProject/UnitTest1.cs
@@ -16,5 +16,32 @@ namespace TestProject
             Assert.AreEqual(result, 9.0);
 
         }
+
+        [TestCase]
+        public void Testsub()
+        {
+            calculation_class calc = new calculation_class();
+            double result = calc.sub(9, 8);
+            Assert.AreEqual(result, 1.0);
+
+        }
+
+        [TestCase]
+        public void Testmult()
+        {
+            calculation_class calc = new calculation_class();
+            double result = calc.mult(2, 2);
+            Assert.AreEqual(result, 4.0);
+
+        }
+
+        [TestCase]
+        public void Testdiv()
+        {
+            calculation_class calc = new calculation_class();
+            double result = calc.div(8, 8);
+            Assert.AreEqual(result, 1.0);
+
+        }
     }
 }
